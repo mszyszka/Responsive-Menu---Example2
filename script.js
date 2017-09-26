2c00d6b5d6a81e371b23f974579a268c66a7fa0b
Michał Szyszka <mszyszka223@gmail.com>
2017-09-26 11:44:06 +0200

    I made rotate for hamburger

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
 function openMenu() {
     document.getElementsByClassName('site-nav')[0].classList.toggle('site-nav-open');
-    
+    document.getElementsByClassName('menu-toggle')[0].classList.toggle('open');
 }
