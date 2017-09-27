34da89e072d2b29ba8c9c8c56465b87e837629ee
Michał Szyszka <mszyszka223@gmail.com>
2017-09-27 10:32:35 +0200

    Rebuilding

--- script.js
+++ script.js
@@ -3,3 +3,7 @@ function openMenu() {
     document.getElementsByClassName('site-nav')[0].classList.toggle('site-nav-open');
     document.getElementsByClassName('menu-toggle')[0].classList.toggle('open');
 }
+
+//$('.menu-toggle').click(function(){
+//    $('.site-nav').toggleClass('site-nav-open');
+//})
